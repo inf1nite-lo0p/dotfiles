4e489087d17ac532d9c3e8fecf133f61375c42e1
Mohammad Ali Shahbazi <mohammadali.shahbazi@gmail.com>
2025-02-26 10:35:36 +0330

    Introduce a better setup and a few aliases

--- setup.sh
+++ setup.sh
@@ -1,5 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Create symbolic links for configuration files
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.bash_logout ~/.bash_logout
+cd "$(dirname "${BASH_SOURCE}")";
+
+git pull origin main;
+
+function doIt() {
+	rsync --exclude ".git/" \
+		--exclude "setup.sh" \
+		--exclude "README.md" \
+		-avh --no-perms . ~;
+	source ~/.bash_profile;
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	doIt;
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt;
+	fi;
+fi;
+unset doIt;
